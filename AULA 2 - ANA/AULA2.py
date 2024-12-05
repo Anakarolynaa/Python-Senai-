7873b18a962e3c1c01be9bf52bf6a2c173bde01c
Anakarolynaa <anakarolyna0063@gmail.com>
2024-12-05 11:25:56 -0300

    APRENDI SOBRE VARIAVEIS

---
+++ AULA 2 - ANA/AULA2.py
@@ -0,0 +1,10 @@
+print('ÓLA MEUS AMORES')
+nome_usuario = 'Anakarolynaa'# tipo -> str = istringS
+idade_usuario = 19 # tipo -> int = integer
+altura_usuario = 1.57 # tipo -> float = flutuante 
+maior_idade = True # tipo -> bool = boolean 
+print('Seja Bem-Vinda', nome_usuario)
+print('sua idade é', idade_usuario, 'anos')
+print('você tem', altura_usuario, 'metros')
+print('você é maior de idade?', maior_idade,)
+
